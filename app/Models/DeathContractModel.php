a44fb5a2934a44f4abf043a42bcc8237e8cd02ce
Diary4 <diarysalah36@gmail.com>
2025-06-03 22:08:07 +0300

    Refactor DeathContractModel to define fillable attributes and establish user relationship

--- app/Models/DeathContractModel.php
+++ app/Models/DeathContractModel.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class DeathContractModel extends Model
 {
-    //
+    protected $fillable = [
+        'gender',
+        'death_location',
+        'date_of_death',
+        'religion',
+        'religion',
+        'nationality',
+        'demander',
+        'location',
+        'related_to_death',
+        'judge',
+        'phone',
+        'witness1_name',
+        'witness2_name',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
