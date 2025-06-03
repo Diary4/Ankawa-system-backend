9b9493d4a89d2c3e028e33198533602d3698f46b
Diary4 <diarysalah36@gmail.com>
2025-06-03 22:14:20 +0300

    Add routes for managing death documents in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MarriageContractController;
-use App\Models\MarriageContract;
+use App\Http\Controllers\DeathDocumentController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,3 +16,7 @@ Route::get('/csrf-token', function () {
     Route::get('/api/marriage-contracts', [MarriageContractController::class, 'index']);
     Route::get('/api/marriage-contracts/{id}', [MarriageContractController::class, 'show']);
     Route::put('/api/marriage-contracts/{id}', [MarriageContractController::class, 'update']);
+
+    Route::get('/api/death-documents', [DeathDocumentController::class, 'index']);
+    Route::get('/api/death-documents/{id}', [DeathDocumentController::class, 'show']);
+    Route::post('/api/death-documents', [DeathDocumentController::class, 'store']);
