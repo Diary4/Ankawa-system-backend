f7adee7cc2b543081bccc335d6eb20531dc89a84
Diary4 <diarysalah36@gmail.com>
2025-06-04 23:49:55 +0300

    Add routes for marriage contracts and death documents management

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MarriageContractController;
+use App\Http\Controllers\DeathDocumentController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/csrf-token', function (Request $request) {
+    return response()->json(['csrfToken' => csrf_token()]);
+});
+
+
+Route::get('/marriage-contracts', [MarriageContractController::class, 'index']);
+Route::get('/marriage-contracts/{id}', [MarriageContractController::class, 'show']);
+Route::post('/marriage-contracts', [MarriageContractController::class, 'store']);
+Route::put('/marriage-contracts/{id}', [MarriageContractController::class, 'update']);
+
+Route::get('death-documents', [DeathDocumentController::class, 'index']);
+Route::get('death-documents/{id}', [DeathDocumentController::class, 'show']);
+Route::post('death-documents', [DeathDocumentController::class, 'store']);
