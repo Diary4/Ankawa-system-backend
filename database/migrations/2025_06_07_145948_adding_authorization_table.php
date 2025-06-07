eb372b662c64a92cc2f96441a082a59ee2fa7ad1
Diary4 <diarysalah36@gmail.com>
2025-06-07 15:02:03 +0300

    Add AuthorizationController, Authorization model, and migration for authorizations table

---
+++ database/migrations/2025_06_07_145948_adding_authorization_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('authorizations', function (Blueprint $table) {
+            $table->id();
+            $table->string('Demander');
+            $table->string('location');
+            $table->text('patient_namea')->nullable();
+            $table->string('disease_type');
+            $table->string('jusdge');
+            $table->string('phone')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
